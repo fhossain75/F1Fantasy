346fc42e16b66e6b1febe2364a0f7a0d771a9a77
Faisal Hossain <faisal.hossain75@gmail.com>
2025-05-07 01:52:36 -0400

    Implemented consideration of DRS 2x chip during team selection algo

--- algo.py
+++ algo.py
@@ -2,6 +2,7 @@
 from itertools import combinations
 import config
 
+
 # Todo: add comments
 # Todo: create test cases
 # Todo: apply double chip
@@ -9,7 +10,7 @@ import config
 # Compute the remaining budget
 # Use DP or backtracking to pick the best 5-driver combination that fits in that remaining budget.
 def best_team_memoization(c_data, d_data, n, budget=config.budget_limit):
-
+    memo = {}
     best_team = []
     best_points = 0
 
@@ -20,7 +21,7 @@ def best_team_memoization(c_data, d_data, n, budget=config.budget_limit):
         constructors_cost = sum(c_data.loc[c_data['Constructor'] == c, 'Cost'].values[0] for c in constructor_combo)
         constructors_points = sum(c_data.loc[c_data['Constructor'] == c, 'Points'].values[0] for c in constructor_combo)
 
-        drivers, drivers_points = drivers_memoization(d_data, n, budget - constructors_cost) #Todo: Reuse memo for optimization?
+        drivers, drivers_points = drivers_memoization(d_data, n, memo, budget - constructors_cost, config.driver_limit, False)
         total_points = constructors_points + drivers_points
 
         if total_points > best_points:
@@ -29,11 +30,8 @@ def best_team_memoization(c_data, d_data, n, budget=config.budget_limit):
 
     return best_team, float(best_points)
 
-def drivers_memoization(driver_data, n, budget=config.budget_limit, limit=config.driver_limit, memo=None):
 
-    # Init memo
-    if not memo:
-        memo = {}
+def drivers_memoization(driver_data, n, memo, budget, limit, chip_used):
 
     # Base Case - end of index or at capacity
     if n == 0 or budget == 0:
@@ -43,32 +41,41 @@ def drivers_memoization(driver_data, n, budget=config.budget_limit, limit=config
             return [], float('-inf')  # invalid team (not enough drivers)
 
     # Optimization: Re-use calculation
-    elif (n, budget, limit) in memo:
-        return memo[(n, budget, limit)]
+    elif (n, budget, limit, chip_used) in memo:
+        return memo[(n, budget, limit, chip_used)]
 
     # Init variables
     index = n - 1
-    points = driver_data["Points"].tolist()[index]
-    cost = driver_data["Cost"].tolist()[index]
-    driver = driver_data["Driver"].tolist()[index]
+    points = driver_data["Points"].values[index]
+    cost = driver_data["Cost"].values[index]
+    driver = driver_data["Driver"].values[index]
 
     # Optimization: Skip if current driver is over budget
     if cost > budget:
-        memo[(n, budget, limit)] = drivers_memoization(driver_data, index, budget, limit, memo)
-        return memo[(n, budget, limit)]
+        memo[(n, budget, limit, chip_used)] = drivers_memoization(driver_data, index, memo, budget, limit, chip_used)
+        return memo[(n, budget, limit, chip_used)]
 
-    # Two choices:
-    sub_team, sub_points = drivers_memoization(driver_data, index, budget - cost, limit - 1, memo)
+    # Choice 1 - Include w/o 2x DRS Chip:
+    sub_team, sub_points = drivers_memoization(driver_data, index, memo, budget - cost, limit - 1, chip_used)
     include_team = sub_team + [driver]
     include_points = sub_points + points
 
-    exclude_team, exclude_points = drivers_memoization(driver_data, index, budget, limit, memo)
+    # Choice 2 - Include w 2x DRS Chip (only if not used yet):
+    if not chip_used:
+        chip_sub_team, chip_sub_points = drivers_memoization(driver_data, index, memo, budget - cost, limit - 1, True)
+        chip_team = chip_sub_team + [driver + "_DRS"]
+        chip_points = chip_sub_points + 2 * points
 
+        if chip_points > include_points:
+            include_team, include_points = chip_team, chip_points
+
+    # Choice 3 - Exclude
+    exclude_team, exclude_points = drivers_memoization(driver_data, index, memo, budget, limit, chip_used)
+
+    # Eval sub-problem
     if include_points > exclude_points:
-        memo[(n, budget, limit)] = include_team, include_points
+        memo[(n, budget, limit, chip_used)] = include_team, include_points
     else:
-        memo[(n, budget, limit)] = exclude_team, exclude_points
+        memo[(n, budget, limit, chip_used)] = exclude_team, exclude_points
 
-    return memo[(n, budget, limit)]
-
-
+    return memo[(n, budget, limit, chip_used)]
